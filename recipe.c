0465171243162b4bf2046871d899f53ee41550cf
hkobo <newtralcydelps@yahoo.co.jp>
2014-06-22 14:59:42 +0900

    spec8: ユーザー名を読み込んで出力する変更

--- recipe.c
+++ recipe.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #define MAX_RECIPE_LETTERS 256
 
 // レシピを標準出力する関数
+// 指定IDが存在しないとき，何も出力されない
 void print_recipes(char recipes[], FILE *recipe_fp, int target_id) {
     int id = 1; // レシピのID
 
@@ -21,9 +23,23 @@ void print_recipes(char recipes[], FILE *recipe_fp, int target_id) {
 	}
 }
 
+// ユーザー名を標準出力する関数
+void print_user_name(char user[]) {
+	printf("ユーザー名: %s\n", user);
+}
+int check_user_name(char user[]) {
+	// ユーザー名が長すぎる場合は偽の値を返す
+	if(strlen(user) > 10) {
+		return 0;
+	}
+	return 1;
+}
+
+
 int main(int argc, char *argv[]){
     FILE *recipe_fp;
     char recipe_string[256];
+	char user_name[10];
 	int received_recipe_id = 0;
 
     recipe_fp = fopen("./recipe-data.txt", "r");
@@ -32,13 +48,27 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
+	// ユーザー名を標準入力
+	printf("input user name: ");
+	scanf("%s", user_name);
+
+	// ユーザー名が正しいかチェック
+	if(check_user_name(user_name)) {
+		// 正しければ何もしない
+	} else {
+		printf("Invalid User Name\n");
+		return 1;
+	}
+
 	// IDを指定されたとき
 	if(argc > 1) {
 		received_recipe_id = atoi(argv[1]);
+		print_user_name(user_name);
 		print_recipes(recipe_string, recipe_fp, received_recipe_id);
 	}
 	// IDが指定されなかったとき
 	else {
+		print_user_name(user_name);
 		print_recipes(recipe_string, recipe_fp, received_recipe_id);
     }
 
