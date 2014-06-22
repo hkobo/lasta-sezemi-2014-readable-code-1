06371e4b0266b75ba9fb0f3a245f901d1223838c
hkobo <newtralcydelps@yahoo.co.jp>
2014-06-22 14:33:20 +0900

    spec6: レシピを出力する関数の追加

--- recipe.c
+++ recipe.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX_DATA_SIZE 256
+#define MAX_RECIPE_LETTERS 256
 
-int count_lines(FILE *fp){
-    int lines = 0;   // 行数
-    char c;      // 改行判定用の文字
-    while((c = getc(fp)) != EOF) {
-        if(c == '\n') lines++;
-    }
-    return lines;
+// レシピを標準出力する関数
+void print_recipes(char recipes[], FILE *recipe_fp, int target_id) {
+    int id = 1; // レシピのID
+
+    while(fgets(recipes, MAX_RECIPE_LETTERS, recipe_fp) != NULL) {
+		// 指定IDのみ標準出力
+		if(id == target_id) {
+       	    printf("%d: ", id);
+           	printf("%s", recipes);
+	    } 
+		// 指定ID以外を標準出力
+		else if(target_id == 0 && id != target_id) {
+       	    printf("%d: ", id);
+           	printf("%s", recipes);
+		}
+		id++;
+	}
 }
 
-
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
     FILE *recipe_fp;
-    char recipe_name[MAX_DATA_SIZE]; // 各行
-    int selected_recipe_id;
-
-    if(argc == 0) {
-        puts("レシピのIDを入力してください");
-        scanf("%d", &selected_recipe_id);
-    } else {
-        selected_recipe_id = atoi(argv[0]);
-    }
+    char recipe_string[256];
+	int received_recipe_id = 0;
 
     recipe_fp = fopen("./recipe-data.txt", "r");
     if(recipe_fp == NULL) {
@@ -30,20 +32,15 @@ int main(int argc, char **argv){
         exit(EXIT_FAILURE);
     }
 
-    int num_recipes = count_lines(recipe_fp); // レシピの個数
-    char recipes[num_recipes][MAX_DATA_SIZE]; // 各レシピ
-    int id = 0; // レシピのID
-    while(fgets(recipe_name, sizeof(recipe_name), recipe_fp) != NULL) {
-        recipes[id] = recipe_name;
-        id++;
-        // printf("%d: ", id++);
-        // printf("%s", recipe_string);
-    while(fgets(recipe_string, 256, recipe_fp) != NULL) {
-        printf("%s", recipe_string);
+	// IDを指定されたとき
+	if(argc > 1) {
+		received_recipe_id = atoi(argv[1]);
+		print_recipes(recipe_string, recipe_fp, received_recipe_id);
+	}
+	// IDが指定されなかったとき
+	else {
+		print_recipes(recipe_string, recipe_fp, received_recipe_id);
     }
-    printf("レシピファイルの行数 = %d\n", num_recipes);
-
-
 
     fclose(recipe_fp);
     return 0;
