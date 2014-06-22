facce886f4bee3432527138ee3c15e546d6ef112
lasta <lastarrow21@gmail.com>
2014-06-22 11:12:14 +0900

    spec3: file open

--- recipe.c
+++ recipe.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(void){
-    puts("オムライス");
+    FILE *recipe_fp;
+
+    recipe_fp = fopen("./recipe-data.txt", "r");
+    if(recipe_fp == NULL) {
+        puts("file open error.");
+        exit(EXIT_FAILURE);
+    } else {
+        puts("file has been opened.");
+    }
+
     return 0;
 }
