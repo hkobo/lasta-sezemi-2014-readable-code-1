69610aa920dccc8eda0c592eb459ace87b93101a
lasta <lastarrow21@gmail.com>
2014-06-22 11:35:57 +0900

    spec6: ファイルの行数を数えようとして失敗した

--- recipe.c
+++ recipe.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int count_lines(FILE *fp){
+    int lines;   // 行数
+    char c;      // 改行判定用の文字
+    while((c = getc(fp)) != EOF) {
+        if(c == '\n') lines++;
+    }
+    return 0;
+}
+
+
 int main(void){
     FILE *recipe_fp;
     char recipe_string[256];
@@ -10,12 +20,15 @@ int main(void){
         puts("file open error.");
         exit(EXIT_FAILURE);
     }
+    int lines = count_lines(recipe_fp);
+    printf("レシピファイルの行数 = %d\n", lines);
 
-    int id = 1; // レシピのID
-    while(fgets(recipe_string, sizeof(recipe_string), recipe_fp) != NULL) {
-        printf("%d: ", id++);
-        printf("%s", recipe_string);
-    }
+    // int id = 1; // レシピのID
+    // while(fgets(recipe_string, sizeof(recipe_string), recipe_fp) != NULL) {
+    //     printf("%d: ", id++);
+    //     printf("%s", recipe_string);
+    // }
+
 
     fclose(recipe_fp);
     return 0;
