a467425b51957296030f9fbe7500cf6642b3de1a
lasta <lastarrow21@gmail.com>
2014-06-22 12:02:18 +0900

    spec6: レシピIDの指定は未実装

--- recipe.c
+++ recipe.c
@@ -1,33 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define MAX_DATA_SIZE 256
 
 int count_lines(FILE *fp){
-    int lines;   // 行数
+    int lines = 0;   // 行数
     char c;      // 改行判定用の文字
     while((c = getc(fp)) != EOF) {
         if(c == '\n') lines++;
     }
-    return 0;
+    return lines;
 }
 
 
-int main(void){
+int main(int argc, char **argv){
     FILE *recipe_fp;
-    char recipe_string[256];
+    char recipe_name[256]; // 各行
+    int selected_recipe_id;
+
+    if(argc == 0) {
+        puts("レシピのIDを入力してください");
+        scanf("%d", &selected_recipe_id);
+    } else {
+        selected_recipe_id = atoi(argv[0]);
+    }
 
     recipe_fp = fopen("./recipe-data.txt", "r");
     if(recipe_fp == NULL) {
         puts("file open error.");
         exit(EXIT_FAILURE);
     }
-    int lines = count_lines(recipe_fp);
-    printf("レシピファイルの行数 = %d\n", lines);
 
-    // int id = 1; // レシピのID
-    // while(fgets(recipe_string, sizeof(recipe_string), recipe_fp) != NULL) {
-    //     printf("%d: ", id++);
-    //     printf("%s", recipe_string);
-    // }
+    int num_recipes = count_lines(recipe_fp); // レシピの個数
+    char recipes[num_recipes]; // 各レシピ
+    int id = 1; // レシピのID
+    while(fgets(recipe_name, sizeof(recipe_name), recipe_fp) != NULL) {
+        recipes[id] = recipe_name;
+        id++;
+    }
+    printf("レシピファイルの行数 = %d\n", num_recipes);
+
 
 
     fclose(recipe_fp);
